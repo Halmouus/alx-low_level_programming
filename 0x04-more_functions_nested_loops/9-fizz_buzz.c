58ecb3a77cc854d0e4287e9c3dccc128002226af
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-03-23 15:21:46 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz game
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i = 1;
+char c[10];
+while (i < 101)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+printf(" ");
+i++;
+}
+printf("$");
+return (0);
+}

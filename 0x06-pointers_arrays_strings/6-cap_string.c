5840897ec5ea080e4ee64b23a26677c904f5460d
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-03-29 17:06:37 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes v2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,12 @@
 char *cap_string(char *str)
 {
 int i = 0;
-while (str[i] != '\0')
+while (str[i])
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
+if (i == 0)
+str[i] -= 32;
 if (i > 0 && (str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ','
 || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '"' ||
 str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||

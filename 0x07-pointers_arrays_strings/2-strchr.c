689eb68e973f75ebfe565bf74afab6cd3b92b2aa
Oussama Oukrid <oukrid.oussama@gmail.com>
2023-04-03 15:23:12 -0700

    2. strchr v2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * _strchr - function that locates a character in a string
@@ -16,6 +16,10 @@ if (*s == c)
 return (s);
 }
 s++;
+}
+if (*s == c)
+{
+return (s);
 }
 return (0);
 }

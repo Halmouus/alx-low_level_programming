8199b4337cfc41641ea3a352f75104a281696e67
Halmouus <javierminimaz@gmail.com>
2023-10-12 00:02:31 +0100

    betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,20 +8,20 @@
  */
 void print_array(const int *array, int start, int end)
 {
-    int i;
-    i = 0;
-    printf("Searching in array: ");
-    while (i <= end)
-    {
-        if (i >= start)
-        {
-            if (i > start)
-                printf(", ");
-            printf("%d", array[i]);
-        }
-        ++i;
-    }
-    printf("\n");
+int i;
+i = 0;
+printf("Searching in array: ");
+while (i <= end)
+{
+if (i >= start)
+{
+if (i > start)
+printf(", ");
+printf("%d", array[i]);
+}
+++i;
+}
+printf("\n");
 }
 
 /**
@@ -30,43 +30,48 @@ void print_array(const int *array, int start, int end)
  * @start: starting index
  * @end: ending index
  * @value: lookup value
+ *
+ * Return: index of the array if found, -1 otherwise
  */
 int binary_search_alg(int *array, int start, int end, int value)
 {
-    int mid;
+int mid;
 
-    if (start <= end) {
-        mid = (start + end) / 2;
-        print_array(array, start, end);
-        
-        if (array[mid] == value) {
-            return mid;
-        }
+if (start <= end)
+{
+mid = (start + end) / 2;
+print_array(array, start, end);
 
-        if (array[mid] > value) {
-            return binary_search_alg(array, start, mid - 1, value);
-        } else {
-            return binary_search_alg(array, mid + 1, end, value);
-        }
-    }
-
-    return (-1);
+if (array[mid] == value)
+{
+return (mid);
+}
+if (array[mid] > value)
+{
+return (binary_search_alg(array, start, mid - 1, value));
+}
+else
+{
+return (binary_search_alg(array, mid + 1, end, value));
+}
+}
+return (-1);
 }
 
 /**
  * binary_search - function that searches for a value in a
  * sorted array of integers using the Binary search algorithm
- * @array - pointer to the first element
+ * @array: pointer to the first element
  * of the array to search in
- * @size - number of elements in array
- * @value - value to search for
+ * @size: number of elements in array
+ * @value: value to search for
  *
  * Return: index where value is located, or -1
  * otherwise
  */
 int binary_search(int *array, size_t size, int value)
 {
-    if (!array)
-        return (-1);
-    return binary_search_alg(array, 0, (int)(size - 1), value);    
+if (!array)
+return (-1);
+return (binary_search_alg(array, 0, (int)(size - 1), value));
 }
